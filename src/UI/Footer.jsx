0fd77383db2f27e74dd96c679e90c85cf7469e69
divyamotghare <divyamotghare9@gmail.com>
2024-08-07 03:09:57 +0530

    footer

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -1,7 +1,22 @@
 function Footer() {
   return (
-    <footer className="w-full bg-purple-l2 py-4">
-      <div className="max-w-[60%] mx-auto px-4"></div>
+    <footer class="bg-booksbg shadow mt-5">
+      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span class="text-sm font-medium text-secondary sm:text-center ">
+          © 2023{" "}
+          <a href="https://.com/" className="hover:underline">
+            kiताbe
+          </a>
+          . All Rights Reserved.
+        </span>
+
+        <a
+          href="https://www.linkedin.com/in/divya-motghare-242278200/"
+          className="hover:underline text-secondary font-medium"
+        >
+          Contact
+        </a>
+      </div>
     </footer>
   );
 }
